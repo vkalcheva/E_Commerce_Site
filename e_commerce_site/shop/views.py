5eaf9761c65357994256025724cbb4c94092d0e0
Violeta Kalcheva <violetakalcheva@abv.bg>
2022-11-21 18:42:35 +0200

    Adding Search Functionality

--- e_commerce_site/shop/views.py
+++ e_commerce_site/shop/views.py
@@ -5,6 +5,12 @@ from e_commerce_site.shop.models import Product
 
 def index(request):
     product_objects = Product.objects.all()
+
+    item_name = request.GET.get('item_name')
+
+    if item_name != '' and item_name is not None:
+        product_objects = product_objects.filter(title__icontains=item_name)
+
     context = {
         'product_objects': product_objects,
     }
